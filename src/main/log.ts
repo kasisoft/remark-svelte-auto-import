504e6adfc271309435754feb75ea20c51210dba7
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2023-11-24 16:58:02 +0100

    + added chalk for some basic logging functionality

---
+++ src/main/log.ts
@@ -0,0 +1,24 @@
+import chalk from 'chalk';
+
+const log = console.log;
+
+function buildMsg(msg: string): string {
+    return chalk.bold('rsai: ') + msg;
+}
+
+export function warn(msg: string) {
+    log(chalk.yellow(buildMsg(msg)));
+}
+
+export function info(msg: string) {
+    log(buildMsg(msg));
+}
+
+export function error(msg: string) {
+    log(chalk.red(buildMsg(msg)));
+}
+
+export function debug(msg: string) {
+    log(chalk.blue(buildMsg(msg)));
+}
+
