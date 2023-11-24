c4239b192ec3f3196da5968fd59afaed5a6afa1d
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2023-11-24 17:03:28 +0100

    + added rollup in order to create a bundled esm version

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "src/main/index.ts",
+  output: [
+    {
+      file: "lib/bundle.esm.js",
+      format: "esm",
+      sourcemap: "inline"
+    }
+  ],
+  external: [
+    "chalk"
+  ],
+  plugins: [
+    resolve(),
+    typescript({"tsconfig": "./tsconfig.json", "filterRoot": "src/main"})
+  ],
+};
+
