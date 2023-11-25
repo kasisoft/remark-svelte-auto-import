718b22322c1db343023a3e3ce15c746c9d8c1280
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2023-11-25 17:32:13 +0100

    ~ formatting

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,20 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "src/main/index.ts",
-  output: [
-    {
-      file: "lib/bundle.esm.js",
-      format: "esm",
-      sourcemap: "inline"
-    }
-  ],
-  external: [
-    "unist-util-visit", "path", "fs", "fast-glob", "chalk", "unified"
-  ],
-  plugins: [
-    resolve(),
-    typescript({"tsconfig": "./tsconfig.json", "filterRoot": "src/main"})
-  ],
+    input: "src/main/index.ts",
+    output: [
+        {
+            file: "lib/bundle.esm.js",
+            format: "esm",
+            sourcemap: "inline"
+        }
+    ],
+    external: [
+        "unist-util-visit", "path", "fs", "fast-glob", "chalk", "unified"
+    ],
+    plugins: [
+        resolve(),
+        typescript({ "tsconfig": "./tsconfig.json", "filterRoot": "src/main" })
+    ],
 };
 
