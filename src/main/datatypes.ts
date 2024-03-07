86dbe2d4151199b0e52995c5493710137efb6c9e
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-03-07 13:38:24 +0100

    ~ made 'Debug' more consistent with other projects and added 'parseDebug' function

--- src/main/datatypes.ts
+++ src/main/datatypes.ts
@@ -1,14 +1,30 @@
 export enum Debug {
-
     None         = 0,
     Default      = 1 << 0,
-    ComponentMap = 1 << 1,
-    RootBefore   = 1 << 2,
-    RootAfter    = 1 << 3,
-    All          = Default | ComponentMap | RootBefore | RootAfter
-
+    RootBefore   = 1 << 1,
+    RootAfter    = 1 << 2,
+    ScriptBefore = 1 << 3,
+    ScriptAfter  = 1 << 4,
+    ComponentMap = 1 << 5,
+    All          = Default | RootBefore | RootAfter | ComponentMap
 } /* ENDENUM */
 
+function parseDebug(val: any): number {
+    if (typeof val === 'string') {
+        switch (val) {
+            case 'None': return Debug.None;
+            case 'Default': return Debug.Default;
+            case 'RootBefore': return Debug.RootBefore;
+            case 'RootAfter': return Debug.RootAfter;
+            case 'ScriptBefore': return Debug.ScriptBefore;
+            case 'ScriptAfter': return Debug.ScriptAfter;
+	    case 'ComponentMap': return Debug.ComponentMap;
+            case 'All': return Debug.All;
+        }
+    }
+    // we know from arktype that it's a number
+    return val as number;
+}
 export interface RemarkSvelteAutoImportOptions {
 
     debug               : Debug;
