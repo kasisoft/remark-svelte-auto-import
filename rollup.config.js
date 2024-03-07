c3c1f1c206a4e24e8ddc945bf7e8fe7132bd6425
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2024-03-07 13:08:10 +0100

    + added @rollup/plugin-alias to use  and  for the both source pathes

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,26 @@
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
+import resolve from '@rollup/plugin-node-resolve';
+import alias from '@rollup/plugin-alias';
+import typescript from '@rollup/plugin-typescript';
 
-export default {
-    input: "src/main/index.ts",
-    output: [
-        {
-            file: "lib/bundle.esm.js",
-            format: "esm",
-            sourcemap: "inline"
-        }
-    ],
-    plugins: [
-        resolve(),
-        typescript({ "tsconfig": "./tsconfig.json", "filterRoot": "src/main" })
-    ],
-};
-
+export default [
+    {
+        input: "src/main/index.ts",
+        output: [
+            {
+                file: "lib/bundle.esm.js",
+                format: "esm",
+                sourcemap: "inline"
+            }
+        ],
+        plugins: [
+            alias({
+                entries: [
+                    { find: '$main', replacement: 'src/main' },
+                    { find: '$test', replacement: 'src/test' }
+                ]
+            }),
+            resolve(),
+            typescript({ "tsconfig": "./tsconfig.json", "filterRoot": "src/main" })
+        ],
+    },
+];
